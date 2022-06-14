b49775ce97c7e14b243605b255eb0edd7b2a4ce2
Omar Keshk <67427221+omarkeshk@users.noreply.github.com>
2022-06-14 20:56:53 +0200

    Added Updated Commands

--- Commands/ban.js
+++ Commands/ban.js
@@ -1,19 +1,19 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-    name : "kick",
+    name : "ban",
     async execute(message, client){
         let args = message.content.split(" ").slice(1);
         let user_id = args[0];
         let reason = args.slice(1).join(" ");
         const user = await client.users.fetch(user_id);
-        user.send(`You were kicked from the server for "${reason}"`)
+        user.send(`You were banned from the server for "${reason}"`)
         .then(()=>{
             const exampleEmbed = new MessageEmbed()
             .setColor('#0099ff')
             // .setTitle('Some title')
             // .setURL('https://discord.js.org/')
-            .setAuthor({ name: 'NGU Bot', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+            .setAuthor({ name: 'NGU Bot', iconURL: 'https://i.imgur.com/AfFp7pu.png'})
             // .setDescription('Some description here')
             // .setThumbnail('https://i.imgur.com/AfFp7pu.png')
             // .addFields(
@@ -23,13 +23,13 @@ module.exports = {
             //     { name: 'Inline field title', value: 'Some value here', inline: true },
             // )
             // .addField('Inline field title', 'Some value here', true)
-            .addField('Member Kicked', `${user.username + '#' + user.discriminator} was kicked from the server for: '${reason}'`)
+            .addField('Member Banned', `${user.username + '#' + user.discriminator} was banned from the server for: '${reason}'`)
             // .setImage('https://i.imgur.com/AfFp7pu.png')
             .setTimestamp()
             // .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
 		    message.channel.send({embeds: [exampleEmbed]})
             .then(()=>{
-                message.guild.members.kick(user_id, reason);
+                message.guild.members.ban(user_id);
             })
         })
     }
