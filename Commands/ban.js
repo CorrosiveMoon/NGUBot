1b7ba6758dbf775068c47d0108b25d2fb3211e3c
Omar Keshk <67427221+omarkeshk@users.noreply.github.com>
2022-06-19 14:58:11 +0200

    Some kick/ban command role fixes
    Allow admins only to use the kick/ban commands and avoid any errors.

--- Commands/ban.js
+++ Commands/ban.js
@@ -1,36 +1,59 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-    name : "ban",
-    async execute(message, client){
+    name: "ban",
+    async execute(message, client) {
         let args = message.content.split(" ").slice(1);
         let user_id = args[0];
         let reason = args.slice(1).join(" ");
-        const user = await client.users.fetch(user_id);
+        if (!user_id) {
+            return message.reply("Please provide a user to ban.");
+        }
+        try {
+            user = await client.users.fetch(user_id);
+            member = await message.guild.members.fetch(user_id);
+        } catch (err) {
+            return message.reply('That user does not exist.');
+        }
+        if (!reason) {
+            return message.reply('You must supply a reason for the ban.');
+        }
+        if (user_id === message.author.id) {
+            return message.reply('You cannot ban yourself.');
+        }
+        if (user_id === client.user.id) {
+            return message.reply('I cannot ban myself.');
+        }
+        if (!user) {
+            return message.reply("That user does not exist.");
+        }
+        if (member.roles.cache.has("923632025058025493")) {
+            return message.reply("You cannot ban an admin.");
+        }
         user.send(`You were banned from the server for "${reason}"`)
-        .then(()=>{
-            const exampleEmbed = new MessageEmbed()
-            .setColor('#0099ff')
-            // .setTitle('Some title')
-            // .setURL('https://discord.js.org/')
-            .setAuthor({ name: 'NGU Bot', iconURL: 'https://i.imgur.com/AfFp7pu.png'})
-            // .setDescription('Some description here')
-            // .setThumbnail('https://i.imgur.com/AfFp7pu.png')
-            // .addFields(
-            //     { name: 'Regular field title', value: 'Some value here' },
-            //     { name: '\u200B', value: '\u200B' },
-            //     { name: 'Inline field title', value: 'Some value here', inline: true },
-            //     { name: 'Inline field title', value: 'Some value here', inline: true },
-            // )
-            // .addField('Inline field title', 'Some value here', true)
-            .addField('Member Banned', `${user.username + '#' + user.discriminator} was banned from the server for: '${reason}'`)
-            // .setImage('https://i.imgur.com/AfFp7pu.png')
-            .setTimestamp()
-            // .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
-		    message.channel.send({embeds: [exampleEmbed]})
-            .then(()=>{
-                message.guild.members.ban(user_id);
+            .then(() => {
+                const exampleEmbed = new MessageEmbed()
+                    .setColor('#0099ff')
+                    // .setTitle('Some title')
+                    // .setURL('https://discord.js.org/')
+                    .setAuthor({ name: 'NGU Bot', iconURL: 'https://raw.githubusercontent.com/CorrosiveMoon/NGUBot/main/BotAvatar.png' })
+                    // .setDescription('Some description here')
+                    // .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+                    // .addFields(
+                    //     { name: 'Regular field title', value: 'Some value here' },
+                    //     { name: '\u200B', value: '\u200B' },
+                    //     { name: 'Inline field title', value: 'Some value here', inline: true },
+                    //     { name: 'Inline field title', value: 'Some value here', inline: true },
+                    // )
+                    // .addField('Inline field title', 'Some value here', true)
+                    .addField('Member Banned', `${user.username + '#' + user.discriminator} was banned from the server for: '${reason}'`)
+                    // .setImage('https://i.imgur.com/AfFp7pu.png')
+                    .setTimestamp()
+                    // .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+                message.channel.send({ embeds: [exampleEmbed] })
+                    .then(() => {
+                        message.guild.members.ban(user_id);
+                    })
             })
-        })
     }
 }
--- Commands/kick.js
+++ Commands/kick.js
@@ -1,36 +1,63 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-    name : "kick",
-    async execute(message, client){
+    name: "kick",
+    async execute(message, client) {
+        if (!message.member.roles.cache.has("923632025058025493")) {
+            return message.reply('You do not have the required role to use this command.');
+        }
         let args = message.content.split(" ").slice(1);
         let user_id = args[0];
         let reason = args.slice(1).join(" ");
-        const user = await client.users.fetch(user_id);
+
+        if (!user_id) {
+            return message.reply("Please provide a user to kick.");
+        }
+        try {
+            user = await client.users.fetch(user_id);
+            member = await message.guild.members.fetch(user_id);
+        } catch (err) {
+            return message.reply('That user does not exist.');
+        }
+        if (!reason) {
+            return message.reply('You must supply a reason for the kick.');
+        }
+        if (user_id === message.author.id) {
+            return message.reply('You cannot kick yourself.');
+        }
+        if (user_id === client.user.id) {
+            return message.reply('I cannot kick myself.');
+        }
+        if (!user) {
+            return message.reply("That user does not exist.");
+        }
+        if (member.roles.cache.has("923632025058025493")) {
+            return message.reply("You cannot kick an admin.");
+        }
         user.send(`You were kicked from the server for "${reason}"`)
-        .then(()=>{
-            const exampleEmbed = new MessageEmbed()
-            .setColor('#0099ff')
-            // .setTitle('Some title')
-            // .setURL('https://discord.js.org/')
-            .setAuthor({ name: 'NGU Bot', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
-            // .setDescription('Some description here')
-            // .setThumbnail('https://i.imgur.com/AfFp7pu.png')
-            // .addFields(
-            //     { name: 'Regular field title', value: 'Some value here' },
-            //     { name: '\u200B', value: '\u200B' },
-            //     { name: 'Inline field title', value: 'Some value here', inline: true },
-            //     { name: 'Inline field title', value: 'Some value here', inline: true },
-            // )
-            // .addField('Inline field title', 'Some value here', true)
-            .addField('Member Kicked', `${user.username + '#' + user.discriminator} was kicked from the server for: '${reason}'`)
-            // .setImage('https://i.imgur.com/AfFp7pu.png')
-            .setTimestamp()
-            // .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
-		    message.channel.send({embeds: [exampleEmbed]})
-            .then(()=>{
-                message.guild.members.kick(user_id, reason);
+            .then(() => {
+                const exampleEmbed = new MessageEmbed()
+                    .setColor('#0099ff')
+                    // .setTitle('Some title')
+                    // .setURL('https://discord.js.org/')
+                    .setAuthor({ name: 'NGU Bot', iconURL: 'https://raw.githubusercontent.com/CorrosiveMoon/NGUBot/main/BotAvatar.png' })
+                    // .setDescription('Some description here')
+                    // .setThumbnail('https://i.imgur.com/AfFp7pu.png')
+                    // .addFields(
+                    //     { name: 'Regular field title', value: 'Some value here' },
+                    //     { name: '\u200B', value: '\u200B' },
+                    //     { name: 'Inline field title', value: 'Some value here', inline: true },
+                    //     { name: 'Inline field title', value: 'Some value here', inline: true },
+                    // )
+                    // .addField('Inline field title', 'Some value here', true)
+                    .addField('Member Kicked', `${user.username + '#' + user.discriminator} was kicked from the server for: '${reason}'`)
+                    // .setImage('https://i.imgur.com/AfFp7pu.png')
+                    .setTimestamp()
+                    // .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+                message.channel.send({ embeds: [exampleEmbed] })
+                    .then(() => {
+                        message.guild.members.kick(user_id, reason);
+                    })
             })
-        })
     }
 }
